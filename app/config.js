b54114ad7f08fe999e3617dde47b1b63f2787cdd
Zulfat <zulfatkin@gmail.com>
2014-09-11 12:37:27 +0400

    temaplate resources urls added

--- app/config.js
+++ app/config.js
@@ -24,22 +24,28 @@
     };
 
     app.value('config', config);
-    
-    app.config(['$logProvider', '$locationProvider', function ($logProvider, $locationProvider) {
+
+    app.config(['$logProvider', '$locationProvider', '$sceDelegateProvider', function ($logProvider, $locationProvider, $sceDelegateProvider) {
         // turn debugging off/on (no info or warn)
         if ($logProvider.debugEnabled) {
             $logProvider.debugEnabled(true);
         }
         $locationProvider.html5Mode(true);
+        $sceDelegateProvider.resourceUrlWhitelist([
+            'http://zulfat.net/feedle/',
+            'http://zulfat.net',
+            'http://www.zulfat.net/feedle/',
+            'http://zulfat.net/feedle/',
+            'self']);
     }])
-       .constant('version', '0.6')
+        .constant('version', '0.6')
 
-       // where to redirect users if they need to authenticate (see module.routeSecurity)
-       .constant('loginRedirectPath', '/login')
+        // where to redirect users if they need to authenticate (see module.routeSecurity)
+        .constant('loginRedirectPath', '/login')
 
-       // your Firebase URL goes here
-       .constant('FBURL', 'https://zulfatfire.firebaseio.com:443');
-    
+        // your Firebase URL goes here
+        .constant('FBURL', 'https://zulfatfire.firebaseio.com:443');
+
     //#region Configure the common services via commonConfig
     app.config(['commonConfigProvider', function (cfg) {
         cfg.config.controllerActivateSuccessEvent = config.events.controllerActivateSuccess;
